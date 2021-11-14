2b8fbd8df0484718db2f27aadb5d76e68c82a8a8
Snehalkhairnar9 <L00163434@student.lyit.ie>
2021-11-14 12:01:21 +0000

    maven program

---
+++ src/main/java/mvnSimplePrj/App.java
@@ -0,0 +1,13 @@
+package mvnSimplePrj;
+/**
+* @author Ruth.Lennon
+*
+*/
+public class App {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+System.out.println("Hello LYIT");
+}
+}
